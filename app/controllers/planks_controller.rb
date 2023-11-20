36eea63ad63c10710b062a91f44df5eb2280a9aa
Maksym Habidulin <habidulin.m@gmail.com>
2023-11-20 16:39:40 +0100

    add_planks_controller

---
+++ app/controllers/planks_controller.rb
@@ -0,0 +1,25 @@
+class PlanksController < ApplicationController
+end
+
+
+# rails g model booking name:string rating:integer
+
+# plank_id:integer
+# renter_user_id:integer
+# start_date:date
+# end_date:date
+# total_price:integer
+# rental_status:char
+# owner_id:integer
+
+
+
+# rails g model plank name:string rating:integer
+
+# user_id:integer
+# name:char
+# description:text
+# price:integer
+# availability_start_date:date
+# availability_end_date:date
+# photo:char
