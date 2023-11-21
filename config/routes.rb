3685001fa601047c9132701587d8f34d18164868
alberto garcia <garcia.lara.alberto@gmail.com>
2023-11-21 15:49:34 +0100

    creates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "home", to: "pages#home"
-
   # Defines the root path route ("/")
-  # root "posts#index"
+
+  root to: "bikes#index"
+
+  resources :bikes, only: [:show, :new, :create] do 
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources  :bookings do
+    resources :reviews, only: [:new, :create]
+  end
 end
