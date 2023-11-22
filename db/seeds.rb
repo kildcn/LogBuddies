aefba845110802bafa48222c6d3447cfe4d35df2
Killian Le Doucen <killian.ledoucen@yahoo.fr>
2023-11-22 15:01:43 +0100

    Seeds were gracefully saved

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,25 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
 # List of users
 User.destroy_all
+Bike.destroy_all
 
-users = [
-  { first_name: 'John', last_name: 'Doe', email: 'john@example.com', password: 'password123' },
-  { first_name: 'Jane', last_name: 'Smith', email: 'jane@example.com', password: 'password123' },
-]
-users.each do |user_attributes|
-  User.create user_attributes
-end
+puts "Cleaning Database 🧼"
+
+puts "Creating users ✌️"
+# Users
+killian = User.create(first_name: 'Killian', last_name: 'Doe', email: 'killian@test.com', password: '123456')
+maksym = User.create(first_name: 'Maksym', last_name: 'Doe', email: 'maksym@test.com', password: '123456')
+alberto = User.create(first_name: 'Alberto', last_name: 'Doe', email: 'alberto@test.com', password: '123456')
+emma = User.create(first_name: 'Emma', last_name: 'Doe', email: 'emma@test.com', password: '123456')
 
 puts "Users created: #{User.count}"
 
-# db/seeds.rb
+puts "Creating bikes .... 🚴🏼‍♀️🚴🏽🚴🏽‍♂️"
 
-# Example bikes
+# Bikes
 bikes = [
   {
     brand: 'Mountain Explorer',
     description: 'A rugged bike suitable for off-road adventures.',
-    user_id: 1,
     price: 15.0,
     availability_start_date: '2023-01-01',
     availability_end_date: '2023-03-31',
@@ -37,7 +28,7 @@ bikes = [
   {
     brand: 'City Cruiser',
     description: 'Perfect for city rides and daily commutes.',
-    user_id: 2,
+    user_id: "2",
     price: 10.0,
     availability_start_date: '2023-04-01',
     availability_end_date: '2023-06-30',
@@ -46,17 +37,27 @@ bikes = [
   {
     brand: 'Speed Racer',
     description: 'A fast bike for those who love speed.',
-    user_id: 3,
+    user_id: killian.id,
     price: 20.0,
     availability_start_date: '2023-07-01',
     availability_end_date: '2023-09-30',
     photo: 'https://example.com/photos/speed_racer.jpg'
   }
-  # ... add more bikes as needed
 ]
 
-# Creating bikes
+emmasbike = Bike.new(brand: 'Speed Racer', description: 'A fast bike for those who love speed.', user_id: emma.id, price: 20.0, availability_start_date: '2023-07-01', availability_end_date: '2023-09-30', photo: 'https://example.com/photos/speed_racer.jpg')
+emmasbike.save!
+
 bikes.each do |bike_attributes|
-  Bike.create bike_attributes
+  Bike.create!(
+    brand: bike_attributes[:brand],
+    description: bike_attributes[:description],
+    price: bike_attributes[:price],
+    user: [killian, emma, alberto, maksym].sample,
+    availability_start_date: '2023-07-01',
+    availability_end_date: '2023-09-30',
+    photo: 'https://example.com/photos/speed_racer.jpg'
+  )
 end
-puts 'Bike seeds created!'
+
+puts "Bike seeds created: #{Bike.count} 🌱"
